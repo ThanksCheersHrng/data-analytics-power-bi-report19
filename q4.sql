bd101cf74f6c6294e9d147d7aae9b5b7101e2f27
ThanksCheersHrng <sistertree321@yahoo.com>
2024-03-13 19:29:28 +0000

    q4's solution table happeend to appear first. so this is q4 done.

---
+++ q4.sql
@@ -0,0 +1,5 @@
+SELECT store_type AS "Store Type",
+       total_sales AS "Total Sales",
+       percentage_of_sales AS "Percentage of Sales",
+       total_orders AS "Total Orders"
+FROM store_type_view;
