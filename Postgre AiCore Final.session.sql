88f3f4cfc984934c2158805c79e40794287700ff
ThanksCheersHrng <sistertree321@yahoo.com>
2024-03-13 18:51:43 +0000

    TAsk2.1 adds list of tables in postgre server db

--- Postgre AiCore Final.session.sql
+++ Postgre AiCore Final.session.sql
@@ -1,4 +1,2 @@
-SELECT column_name
-FROM information_schema.columns
-WHERE table_schema = 'public'
-  AND table_name = 'orders';
+SELECT table_name FROM information_schema.tables 
+    WHERE table_schema = 'public'
